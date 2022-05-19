e23783e125f512f680a433d08c21fde33df0ca94
Domianidze <sdomianidze123@gmail.com>
2022-05-19 15:20:23 +0400

    style: create set password page

--- src/pages/SetPassword/SetPassword.tsx
+++ src/pages/SetPassword/SetPassword.tsx
@@ -1,5 +1,28 @@
-const SetPassword = () => {
-  return <h2>Set Password</h2>;
+import Title from 'components/Title';
+import Input from 'components/Input';
+import Button from 'components/Button';
+
+const SavePassword = () => {
+  return (
+    <form>
+      <Title value='Reset Password' />
+      <Input
+        type='password'
+        label='New password'
+        placeholder='Enter new password'
+        id='password'
+        classname='pt-10'
+      />
+      <Input
+        type='password'
+        label='Repeat password'
+        placeholder='Repeat password'
+        id='repeat-password'
+        classname='pb-10'
+      />
+      <Button type='submit' value='Save Changes' />
+    </form>
+  );
 };
 
-export default SetPassword;
+export default SavePassword;
