e94ebd8955d32fe30cf8c492d1376ddbc65f8b0f
Domianidze <sdomianidze123@gmail.com>
2022-05-21 20:33:59 +0400

    feat: create set password validation

--- src/pages/SetPassword/SetPassword.tsx
+++ src/pages/SetPassword/SetPassword.tsx
@@ -1,17 +1,43 @@
+import { useForm } from 'react-hook-form';
+
 import Title from 'components/Title';
 import Input from 'components/Input';
 import Button from 'components/Button';
 
 const SavePassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, touchedFields },
+    watch,
+  } = useForm({
+    mode: 'onTouched',
+  });
+
+  const setPasswordHandler = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
-    <form>
-      <Title value='Reset Password' className='center' />
+    <form onSubmit={setPasswordHandler}>
+      <Title value='Reset Password' className='text-center' />
       <Input
         type='password'
         label='New password'
         placeholder='Enter new password'
         id='password'
         classname='pt-10'
+        register={{
+          ...register('password', {
+            required: 'Password is required',
+            minLength: {
+              value: 3,
+              message: 'Password has to contain at least 3 symbols',
+            },
+          }),
+        }}
+        error={errors?.password?.message}
+        isTouched={touchedFields?.password}
       />
       <Input
         type='password'
@@ -19,6 +45,16 @@ const SavePassword = () => {
         placeholder='Repeat password'
         id='repeat-password'
         classname='pb-10'
+        register={{
+          ...register('confirmPassword', {
+            required: 'Repeating password is required',
+            validate: (value) => {
+              return value === watch('password') || 'Passwords must match';
+            },
+          }),
+        }}
+        error={errors?.confirmPassword?.message}
+        isTouched={touchedFields?.confirmPassword}
       />
       <Button type='submit' value='Save Changes' />
     </form>
