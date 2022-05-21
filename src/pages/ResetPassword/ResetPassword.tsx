273d8eecb9875498342d1db2aa5de8de983e6911
Domianidze <sdomianidze123@gmail.com>
2022-05-21 20:28:36 +0400

    feat: create reset password validation

--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -1,10 +1,24 @@
+import { useForm } from 'react-hook-form';
+
 import Title from 'components/Title';
 import Input from 'components/Input';
 import Button from 'components/Button';
 
 const ResetPassword = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, touchedFields },
+  } = useForm({
+    mode: 'onTouched',
+  });
+
+  const resetPasswordHandler = handleSubmit((data) => {
+    console.log(data);
+  });
+
   return (
-    <form>
+    <form onSubmit={resetPasswordHandler}>
       <Title value='Reset Password' className='text-center' />
       <Input
         type='email'
@@ -12,6 +26,17 @@ const ResetPassword = () => {
         placeholder='Enter your email'
         id='email'
         classname='py-10'
+        register={{
+          ...register('email', {
+            required: 'Email is required',
+            pattern: {
+              value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+              message: 'Email must be valid',
+            },
+          }),
+        }}
+        error={errors?.email?.message}
+        isTouched={touchedFields?.email}
       />
       <Button type='submit' value='Reset Password' />
     </form>
